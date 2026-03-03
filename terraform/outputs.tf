5d454e5b578a638abbe7313da3328c8be0336e82
gnnoroum <gnnoroum@gmail.com>
2026-03-03 15:33:59 -0500

    feat(terraform): add infrastructure outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,19 @@
+output "network_name" {
+  value = google_compute_network.vpc.name
+}
 
+output "subnet_name" {
+  value = google_compute_subnetwork.subnet.name
+}
+
+output "vm_name" {
+  value = google_compute_instance.epic_interconnect_vm.name
+}
+
+output "vm_internal_ip" {
+  value = google_compute_instance.epic_interconnect_vm.network_interface[0].network_ip
+}
+
+output "vm_external_ip" {
+  value = try(google_compute_instance.epic_interconnect_vm.network_interface[0].access_config[0].nat_ip, null)
+}
